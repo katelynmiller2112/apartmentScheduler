9f717ac53fbcf5bfc8243164b93172d26d62acf0
Miller <millerk9@wit.edu>
2021-08-16 20:45:59 -0400

    continuing login work

---
+++ src/Dashboard/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Jumbotron, } from 'react-bootstrap';
+
+
+class Dashboard extends React.Component{
+    
+    render(){
+        return(
+            <div>
+            <Jumbotron></Jumbotron>
+            </div>
+        );
+
+    };
+}
+
+export default Dashboard;
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,26 +3,42 @@ import {Button, FormText} from 'react-bootstrap';
 import * as redux from 'react-redux';
 import styles from './login.module.css';
 
-//eslint
-
-var styleObj = {
-
-    fontSize: 14,
-
-    color: "#4a54f1",
-
-    textAlign: "center",
-
-    paddingTop: "100px",
-
-}
 class LoginPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "",
+            password: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        
+    };
 
+    
+    onUsernameChange = event => {
+        this.setState({ userName: event.target.value });
+    }
+    onPasswordChange = event => {
+        this.setState({ password: event.target.value })
+        
+    }
+    handleChange = (event) => {
+        console.log(this.state);
+        this.setState({ userName: event.target.value });
+    }
+onLogin = () => {
+        //send login data to backend
+};
     render() {
+        
         return (
-            <div>
-                <p style={styleObj}>Hello World!!!</p>
-                
+            <div className={styles.text}>
+                <p>Login!</p>
+                <p>Username</p>
+                <input type="text" onChange={this.handleChange}></input>
+                <p>Password</p>
+                <input type="text"  onChange={ this.handleChange}></input>
+                <Button><p>login</p></Button>
             </div>
         );
     }
